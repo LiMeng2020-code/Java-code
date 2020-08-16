620fe8b7edb6ca5245bd5d98400366aa24819b42
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-08-16 10:18:22 +0800

    Add files via upload

---
+++ Demo01Extends.java
@@ -0,0 +1,8 @@
+package cn.itcast.day09.demo01;
+
+public class Demo01Extends {
+    public static void main(String[] args) {
+        Teacher teacher01=new Teacher();
+        teacher01.method();
+    }
+}
