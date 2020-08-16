c7c030fe9d07f65f4225ab8ac7fe9a007c98f997
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-08-16 10:15:34 +0800

    Add files via upload

---
+++ Demo03Math.java
@@ -0,0 +1,9 @@
+package Demo04;
+
+public class Demo03Math {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(12));
+        System.out.println(Math.abs(1.2));
+        System.out.println(Math.abs(-2));
+    }
+}
