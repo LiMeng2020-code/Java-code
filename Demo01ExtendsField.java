b6d99b069e75eaceac4975f525bf7c399264ec38
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-08-17 14:45:37 +0800

    Add files via upload

---
+++ Demo01ExtendsField.java
@@ -0,0 +1,10 @@
+package cn.itcast.day09.demo02;
+
+public class Demo01ExtendsField {
+    public static void main(String[] args) {
+        Fu fu01=new Fu();
+        System.out.println(fu01.numFu);
+        Zi zi01=new Zi();
+        System.out.println(zi01.numFu);
+    }
+}
