4bf03bdb174cf59eaebc10d931f3949a1558a8e3
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-08-19 10:59:15 +0800

    Add files via upload

---
+++ Demo01show.java
@@ -0,0 +1,10 @@
+package cn.itcast.day09.Demo06;
+
+import cn.itcast.day09.Demo04.Zi;
+
+public class Demo01show {
+    public static void main(String[] args) {
+        PhoneNew phone01=new PhoneNew();
+        phone01.show();
+    }
+}
