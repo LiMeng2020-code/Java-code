8cf7951ad5e86db8e3c8e041244883c091a21b59
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-08-21 09:31:30 +0800

    Add files via upload

--- Zi.java
+++ Zi.java
@@ -1,9 +1,16 @@
-package cn.itcast.day09.Demo07;
+package cn.itcast.day09.Demo08;
 
 public class Zi extends Fu{
-    public Zi(){
-        super(9);
-        System.out.println("zilei");
-
+    int num=20;
+    @Override
+    public void method(){
+       // int num=30;
+        System.out.println("zilei01");
+    }
+    public void show(){
+        int num=30;
+        System.out.println(num);
+        System.out.println(this.num);
+        System.out.println(super.num);
     }
 }
