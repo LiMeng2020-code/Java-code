7c3cd4acf8a3675e1a16528f90494091e9b6a5b2
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-08-23 22:21:10 +0800

    Add files via upload

---
+++ Demo02Interface.java
@@ -0,0 +1,9 @@
+package cn.itcast.day10.Demo01;
+
+public class Demo02Interface {
+    public static void main(String[] args) {
+        MyInterfaceDefaultA interfaceDefault01=new MyInterfaceDefaultA();
+        interfaceDefault01.methodAbs();
+        interfaceDefault01.methodAbs2();
+    }
+}
