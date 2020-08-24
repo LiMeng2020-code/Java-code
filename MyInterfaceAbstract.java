db79fdb68a25e4f7a8f4bb57009d546c3767ef85
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-08-24 09:44:38 +0800

    Add files via upload

---
+++ MyInterfaceAbstract.java
@@ -0,0 +1,9 @@
+package cn.itcast.day10.Demo01;
+
+public interface MyInterfaceAbstract {
+    public abstract void method01();
+    public abstract void method02();
+    public abstract void method03();
+    public abstract void method04();
+
+}
