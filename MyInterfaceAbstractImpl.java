30bee88efc25f4b7ff27d411aff2c47dda41ef64
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-08-24 09:44:58 +0800

    Add files via upload

---
+++ MyInterfaceAbstractImpl.java
@@ -0,0 +1,26 @@
+package cn.itcast.day10.Demo01;
+
+public class MyInterfaceAbstractImpl implements MyInterfaceAbstract{
+    @Override
+    public void method01() {
+        System.out.println("fanga01");
+    }
+
+    @Override
+    public void method02() {
+        System.out.println("fangfa02");
+
+    }
+
+    @Override
+    public void method03() {
+        System.out.println("fangfa03");
+
+    }
+
+    @Override
+    public void method04() {
+        System.out.println("fangfa04");
+
+    }
+}
