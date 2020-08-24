70c06ca0f4375787688f4e072ed207e213be9ef4
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-08-24 09:43:46 +0800

    Add files via upload

---
+++ Demo04Interfaceprivate.java
@@ -0,0 +1,9 @@
+package cn.itcast.day10.Demo01;
+
+public class Demo04Interfaceprivate {
+    public static void main(String[] args) {
+       // MyInterfacePrivateimpl myInterfacePrivate01=new MyInterfacePrivateimpl();
+        MyInterfacePrivateB.methodDefault01();
+        MyInterfacePrivateB.methodDefault02();
+    }
+}
