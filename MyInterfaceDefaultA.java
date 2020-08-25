ee98c6283bc7148fd475d2a3f363d85fa095d4c3
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-08-26 07:37:26 +0800

    Add files via upload

---
+++ MyInterfaceDefaultA.java
@@ -0,0 +1,7 @@
+package cn.itcast.day10.Demo01;
+
+public class MyInterfaceDefaultA implements MyInterfaceDefault{
+    public void methodAbs(){
+        System.out.println("shixianl jiekouA");
+    }
+}
