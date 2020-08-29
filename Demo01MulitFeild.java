87d15f516e0305c51bab7384573cfcf34c54bd03
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-08-29 08:10:48 +0800

    Add files via upload

---
+++ Demo01MulitFeild.java
@@ -0,0 +1,10 @@
+package cn.itcast.day10.Demo05;
+
+public class Demo01MulitFeild {
+    public static void main(String[] args) {
+        Fu fu01=new Zi();
+        System.out.println(fu01.num);
+        //System.out.println(fu01.age);
+        fu01.method();
+    }
+}
