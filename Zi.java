871a77a3e70a6081931067c44119e3f0b59820b2
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-08-30 22:11:11 +0800

    Add files via upload

--- Zi.java
+++ Zi.java
@@ -1,7 +1,12 @@
-package cn.itcast.day10.Demo04;
+package cn.itcast.day10.Demo05;
 
 public class Zi extends Fu{
+    int num=20;
+    int age=20;
     public void method(){
-        System.out.println("zilei");
+        System.out.println(num);
+    }
+    public void chifan(){
+        System.out.println("chifan");
     }
 }
