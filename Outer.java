a50a6be1ec3a63c7854e67c2c37bff253712da8b
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-09-01 07:47:48 +0800

    Add files via upload

---
+++ Outer.java
@@ -0,0 +1,14 @@
+package cn.itcast.day11.Demo03;
+
+public class Outer {
+    int num=10;
+    public class Inner{
+        int num=20;
+        public void method(){
+            int num=30;
+            System.out.println(num);
+            System.out.println(this.num);
+            System.out.println(Outer.this.num);
+        }
+    }
+}
