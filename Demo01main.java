1592e4b1f039f428aabbb6ae9688895e03ad5009
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-09-01 07:48:51 +0800

    Add files via upload

--- Demo01main.java
+++ Demo01main.java
@@ -1,4 +1,33 @@
-package cn.itcast.day11.Demo02;
+package cn.itcast.day11.Demo05;
 
 public class Demo01main {
+    public static void main(String[] args) {
+        Myinterfaceimpl impl=new Myinterfaceimpl();
+        impl.method1();
+        Myinterface OBJ=new Myinterface() {
+            @Override
+            public void method1() {
+                System.out.println("niming neibulei");
+            }
+            @Override
+            public void method2() {
+                System.out.println("niming neibulei");
+            }
+        };
+        OBJ.method1();
+        OBJ.method2();
+        new Myinterface(){
+            @Override
+            public void method1() {
+                System.out.println("niming neibulei");
+            }
+            @Override
+            public void method2() {
+                System.out.println("niming neibulei");
+            }
+        }.method1();
+    }
+
+
+
 }
