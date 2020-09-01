4219bde29a3a87d72b63c3139ef16154946852de
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-09-02 07:25:40 +0800

    Add files via upload

--- Hero.java
+++ Hero.java
@@ -1,45 +1,34 @@
-package cn.itcast.day11.Demo06;
+package cn.itcast.day11.Demo07;
 
 public class Hero {
     private String name;
-    private int age;
-    private Weapon weapon;
-
-
-
-    public Hero(String name, int age, Weapon weapon) {
-        this.name = name;
-        this.age = age;
-        this.weapon = weapon;
+    private Skill skill;
+    public void attack(){
+        System.out.println(name);
+        skill.use();
     }
 
-    public Hero() {
-    }
-    public Hero(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
-    public int getAge() {
-        return age;
-    }
-    public void setAge(int age) {
-        this.age = age;
 
-    }
-    public Weapon getWeapon() {
-        return weapon;
+    public Skill getSkill() {
+        return skill;
     }
 
-    public void setWeapon(Weapon weapon) {
-        this.weapon = weapon;
+    public void setSkill(Skill skill) {
+        this.skill = skill;
     }
-    public void attack(){
-        System.out.println(name+age+weapon.getCode());
+
+    public Hero(String name, Skill skill) {
+        this.name = name;
+        this.skill = skill;
+    }
+
+    public Hero() {
     }
 }
