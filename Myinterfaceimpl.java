72ff3cb2a42a43b941d624577efe48cce1112773
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-09-02 07:22:45 +0800

    Add files via upload

---
+++ Myinterfaceimpl.java
@@ -0,0 +1,15 @@
+package cn.itcast.day11.Demo05;
+
+public class Myinterfaceimpl implements Myinterface{
+
+    @Override
+    public void method1() {
+        System.out.println("chongxie1");
+    }
+
+    @Override
+    public void method2() {
+        System.out.println("chongxie2");
+
+    }
+}
