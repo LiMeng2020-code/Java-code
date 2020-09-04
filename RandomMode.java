9e3f1dbb81ba56be1e3e3d70661db2b758d11080
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-09-04 23:39:38 +0800

    Add files via upload

---
+++ RandomMode.java
@@ -0,0 +1,12 @@
+package cn.itcast.day11.Demo08;
+
+import java.util.ArrayList;
+
+public class RandomMode implements OpenMode {
+    @Override
+    public ArrayList<Integer> devide(int totalmoney, int count) {
+        ArrayList<Integer>list=new ArrayList<>();
+
+        return list;
+    }
+}
