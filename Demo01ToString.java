c3cd93145be3b9f65b4671c057d71c84eb36548a
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-09-05 08:49:57 +0800

    Add files via upload

---
+++ Demo01ToString.java
@@ -0,0 +1,10 @@
+package cn.itcast.day12.Demo01;
+
+public class Demo01ToString {
+    public static void main(String[] args) {
+        Person person=new Person("lm",10);
+        System.out.println(person.toString());
+        System.out.println(person);
+
+    }
+}
