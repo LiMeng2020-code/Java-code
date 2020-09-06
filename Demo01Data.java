e5a6b891db1f10273a6532ac00fe307ae938b5ea
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-09-06 12:22:26 +0800

    Add files via upload

---
+++ Demo01Data.java
@@ -0,0 +1,7 @@
+package cn.itcast.day12.Demo02;
+import java.util.Date;
+public class Demo01Data {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+    }
+}
