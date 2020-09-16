ca1589042f1aeffa09a1016e4b137d772475ad5d
Meng Li <69453859+LiMeng2020-code@users.noreply.github.com>
2020-09-16 09:53:38 +0800

    Add files via upload

---
+++ demo03genericmethod.java
@@ -0,0 +1,12 @@
+package cn.itcast.day13.demo02;
+
+public class demo03genericmethod {
+    public static void main(String[] args) {
+        GenericMethod GM=new GenericMethod();
+        GM.GenericMethod(1);
+        GM.GenericMethod("LM");
+        GM.method02(3);
+        GenericMethod.method02("123");
+
+    }
+}
